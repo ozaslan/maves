18b6b7fdfe6f94810ffe04762a9ccc35ca19343d
Ozaslan <ozaslan@seas.upenn.edu>
2025-12-20 21:32:37 +0300

    removed stduent code and arenas

--- matlab/scenarios/readme.m
+++
@@ -1,9 +0,0 @@
-Which way points to follow
-- time to visit each are to be defined as well
-What trajectory generator to use
-Which drone to be used 
-- a drone is defined by its parameters such as mass, motor type etc.
-What low-level controller to be used
-What solver to be used
-What controller to use
-- also what parameters to use for that specific controller
