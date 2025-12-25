542c66ec6f90bc9585f18034f738d0ea29b2fead
Ozaslan <tozaslan@ybu.edu.tr>
2025-12-25 17:48:51 +0300

    Simplify trajectory parameter handling

--- matlab/runsim.m
+++ matlab/runsim.m
@@ -3,33 +3,7 @@ addpath('./core');
 addpath('./utils');
 addpath('./scenarios');
 
-% Exactly one solution directory should be active at a time so that the
-% simulator can locate the corresponding controller and trajectory
-% generators. Select the desired submission by setting the label below.
-% Use 'student' for the starter code provided in this repository.
-activeSolution = 'solution';
-
-solutionPaths = {
-    'student', './studentCode';
-    'solution', './soln';
-};
-
-solutionLabels = solutionPaths(:, 1);
-if ~ismember(activeSolution, solutionLabels)
-    error('Unknown submission "%s". Update activeSubmission to one of: %s', ...
-        activeSolution, strjoin(solutionLabels', ', '));
-end
-
-for idx = 1:size(solutionPaths, 1)
-    solutionDir = solutionPaths{idx, 2};
-    if strcmp(solutionPaths{idx, 1}, activeSolution)
-        addpath(solutionDir);
-    else
-        try
-            rmpath(solutionDir);
-        end
-    end
-end
+addpath('./soln');
 
 % Pick which scenario to run. If no overrides are provided, the default
 % line trajectory with the reference LQR controller will be used.
@@ -40,7 +14,7 @@ scenario = chooseScenario(activeScenario);
 initialize();
 respawn();
 
-registerTrajectory(scenario.trajHandle, scenario.trajParams);
+setTrajectoryGenerator(scenario.trajHandle, scenario.trajParams);
 setController(scenario.controllerHandle, scenario.controllerParams);
 setVisualizationMode(scenario.visualizationMode);
 setCaptureMode(scenario.captureMode);
@@ -59,22 +33,3 @@ fprintf('Simulation Stopped!\n');
 updateVisuals(true);
 drawnow();
 
-function registerTrajectory(trajHandle, trajParams)
-%REGISTERTRAJECTORY Configure the selected trajectory generator.
-
-switch func2str(trajHandle)
-    case 'trajHover'
-        setTrajectoryGenerator(trajHandle, trajParams);
-    case 'trajLine'
-        setTrajectoryGenerator(trajHandle, trajParams.posStart, ...
-            trajParams.posEnd, trajParams.tEnd);
-    case 'trajDiamond'
-        setTrajectoryGenerator(trajHandle, trajParams.p0, trajParams.p1, ...
-            trajParams.p2, trajParams.p3, trajParams.tEnd);
-    case 'trajCircle'
-        setTrajectoryGenerator(trajHandle, trajParams.center, ...
-            trajParams.radius, trajParams.tEnd);
-    otherwise
-        error('Unsupported trajectory generator: %s', func2str(trajHandle));
-end
-end
--- matlab/utils/setTrajectoryGenerator.m
+++ matlab/utils/setTrajectoryGenerator.m
@@ -1,6 +1,6 @@
 function setTrajectoryGenerator(h, varargin)
 %SETTRAJECTORYGENERATOR Use this function to set traj. generator
-%   
+%
 %   The input must be a handle to a trajectory generator function.
 %   The input - output specifications of a generator function are given in
 %   template functions. See 'trajHover', 'trajLine', 'trajDiamond',
@@ -8,6 +8,21 @@ function setTrajectoryGenerator(h, varargin)
 
 global params;
 
-h = @(t, s) h(t, s, varargin{:});
+if isempty(varargin)
+    trajArgs = {};
+elseif numel(varargin) == 1
+    candidate = varargin{1};
+    if isstruct(candidate)
+        trajArgs = struct2cell(candidate);
+    elseif iscell(candidate)
+        trajArgs = candidate;
+    else
+        trajArgs = {candidate};
+    end
+else
+    trajArgs = varargin;
+end
+
+h = @(t, s) h(t, s, trajArgs{:});
 params.trajGen = h;
 
